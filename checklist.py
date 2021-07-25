b22656e89f63ae4b57b6925e7aa3040111fa7912
zarexalvindaria <zarexalvindaria@users.noreply.github.com>
2021-07-25 08:11:13 +0800

    added the scrollbar of the output frame

--- checklist.py
+++ checklist.py
@@ -17,6 +17,7 @@ root.config(bg=root_color)
 # Define functions
 
 
+
 # Define layout
 # Create frames
 input_frame = tk.Frame(root, bg=root_color)
@@ -29,22 +30,24 @@ button_frame.pack()
 # Input frame layout
 list_entry = tk.Entry(input_frame, width=35, borderwidth=3, font=my_font)
 list_add_button = tk.Button(input_frame, text="Add Item", borderwidth=2, font=my_font, bg=button_color)
-list_entry.grid(row=0, column=0)
-list_add_button.grid(row=0, column=1)
+list_entry.grid(row=0, column=0, padx=5, pady=5)
+list_add_button.grid(row=0, column=1, padx=5, pady=5, ipadx=5)
 
 # Output frame layout
+my_scrollbar = tk.Scrollbar(output_frame)
 my_listbox = tk.Listbox(output_frame, height=15, width=45, borderwidth=3, font=my_font)
-my_listbox.grid(row=0, column=9)
+my_listbox.grid(row=0, column=0)
+my_scrollbar.grid(row=0, column=1, sticky="NS")
 
 # Button Frame layout
 list_remove_button = tk.Button(button_frame, text="Remove Item", borderwidth=2, font=my_font, bg=button_color)
 list_clear_button = tk.Button(button_frame, text="Clear List", borderwidth=2, font=my_font, bg=button_color)
 save_button = tk.Button(button_frame, text="Save List", borderwidth=2, font=my_font, bg=button_color)
 quit_button = tk.Button(button_frame, text="Quit", borderwidth=2, font=my_font, bg=button_color, command=root.destroy)
-list_remove_button.grid(row=0, column=0)
-list_clear_button.grid(row=0, column=1)
-save_button.grid(row=0, column=2)
-quit_button.grid(row=0, column=3)
+list_remove_button.grid(row=0, column=0, padx=2, pady=10)
+list_clear_button.grid(row=0, column=1, padx=2, pady=10, ipadx=10)
+save_button.grid(row=0, column=2, padx=2, pady=10, ipadx=10)
+quit_button.grid(row=0, column=3, padx=2, pady=10, ipadx=25)
 
 # Run the root window
 root.mainloop()
